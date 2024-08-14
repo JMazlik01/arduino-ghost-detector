55e0279f29141fdef09f60c0dd67082f7c8e63d2
JMazlik01 <jakub.mazel@icloud.com>
2024-08-14 15:04:40 +0200

    Code improvements

--- ghost_detector.ino
+++ ghost_detector.ino
@@ -1,32 +1,32 @@
 
-int const potPin = A0;
+int const POT_PIN = A0;
+int const LED_COUNT = 6;
+
 int potVal = 0;
-int ghostC = 0;
+int pinsOn = 0;
 int blinkDelay = 250;
 int blink = LOW;
 bool turnedOn = false;
 
+int const ledPins[LED_COUNT] = { 2, 3, 4, 5, 6, 7 };
+int const blinkDelays[LED_COUNT + 1] = { 500, 500, 400, 300, 250, 150, 50 };
 
 void setup() {
 
-  // GREEN LEDs
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  for (int i = 0; i < LED_COUNT; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 
-  // ORANGE LEDs
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-
-  // RED LED
-  pinMode(7, OUTPUT);
 }
 
 void loop() {
 
-  potVal = analogRead(potPin);
-  ghostC = map(potVal, 0, 1023, 0, 7);
-  turnedOn = ghostC != 0;
+  potVal = analogRead(POT_PIN);
+
+  // 0 - LED_COUNT + 1 -> +1 for the 'turned off' state
+  pinsOn = map(potVal, 0, 1023, 0, LED_COUNT);
+  turnedOn = pinsOn != 0;
+  blinkDelay = blinkDelays[pinsOn];
 
   if (turnedOn && blink == LOW) {
     tone(8, 440);
@@ -36,92 +36,20 @@ void loop() {
     noTone(8);
   }
 
-  switch (ghostC) {
 
-    case 0:
-    
-      // TURNED OFF
-      digitalWrite(2, LOW);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      break;
+  for (int i = 0; i < LED_COUNT; i++) {
+      int pin = ledPins[i];
 
-    case 1:
-      
-      // FIRST GREEN LED
-      digitalWrite(2, blink);
-      digitalWrite(3, LOW);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-
-      blinkDelay = 500;
-      break;
-    case 2:
-
-      // SECOND GREEN LED
-      digitalWrite(2, HIGH);
-      digitalWrite(3, blink);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-
-      blinkDelay = 400;
-      break;
-    case 3:
-
-      // THIRD GREEN LED
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, blink);
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-
-      blinkDelay = 300;
-      break;
-    case 4:
-
-      // FIRST ORANGE LED
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, blink);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-
-      blinkDelay = 250;
-      break;
-    case 5:
-
-      // SECOND ORANGE LED
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, blink);
-      digitalWrite(7, LOW);
-
-      blinkDelay = 150;
-      break;
-    default:
-
-      // RED LED
-      digitalWrite(2, HIGH);
-      digitalWrite(3, HIGH);
-      digitalWrite(4, HIGH);
-      digitalWrite(5, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(7, blink);
-
-      blinkDelay = 50;
-      break;
-  }
+      if (i < pinsOn - 1) {
+        digitalWrite(pin, HIGH);
+      } 
+      else if (i == pinsOn - 1) {
+        digitalWrite(pin, blink);
+      }
+      else {
+        digitalWrite(pin, LOW);
+      }
+    }
 
   delay(blinkDelay);
 }
